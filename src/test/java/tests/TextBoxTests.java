2b5ec4441083f445111c455cd631c4c10bc05a51
Aleksandra Ivanova <ival96vl@gmail.com>
2026-06-02 00:32:25 +0300

    Added TestBase

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -8,16 +8,8 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
-    @BeforeAll
-    static void beforeAll () {
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "144.0";
-        Configuration.browserSize = "1920*1080";
-        Configuration.baseUrl = "https://demoqa.com";
- //       Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 1000; // default 4000
-    }
+public class TextBoxTests extends TestBase {
+
 
     @Test
     void successfulFillFormTest () {
